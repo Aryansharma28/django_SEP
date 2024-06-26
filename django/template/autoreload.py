e458112796b0f3014f089149d8bfcb46cfe93ed8
Claudia Montalvo <mclaudiamontalvo@gmail.com>
2024-06-26 18:06:33 +0200

    Coverage Clau Autoreload

--- django/template/autoreload.py
+++ django/template/autoreload.py
@@ -6,6 +6,15 @@ from django.template.backends.django import DjangoTemplates
 from django.utils._os import to_path
 from django.utils.autoreload import autoreload_started, file_changed, is_django_path
 
+#data structure to hold coverage information about the conditional branches
+coverage_info = {
+    "backend_not_django_templates": False,
+    "update_items_dirs": False,
+    "loader_not_get_dirs": False,
+    "update_items_loader_dirs": False,
+    "loader_is_get_dirs": False,
+    "update_items_filtered_dirs": False,
+}
 
 def get_template_directories():
     # Iterate through each template backend and find
@@ -15,18 +24,24 @@ def get_template_directories():
     items = set()
     for backend in engines.all():
         if not isinstance(backend, DjangoTemplates):
+            coverage_info["backend_not_django_templates"] = True
             continue
 
         items.update(cwd / to_path(dir) for dir in backend.engine.dirs if dir)
+        coverage_info["update_items_dirs"] = True
 
         for loader in backend.engine.template_loaders:
             if not hasattr(loader, "get_dirs"):
+                coverage_info["loader_not_get_dirs"] = True
                 continue
             items.update(
                 cwd / to_path(directory)
                 for directory in loader.get_dirs()
                 if directory and not is_django_path(directory)
             )
+            coverage_info["loader_is_get_dirs"] = True
+            coverage_info["update_items_loader_dirs"] = True
+            coverage_info["update_items_filtered_dirs"] = True
     return items
 
 
@@ -59,3 +74,8 @@ def template_changed(sender, file_path, **kwargs):
         if template_dir in file_path.parents:
             reset_loaders()
             return True
+
+def print_coverage_info():
+    print("Coverage Information:")
+    for branch, executed in coverage_info.items():
+        print(f"{branch}: {'Executed' if executed else 'Not Executed'}")
